b9aeecc980bb92fe399a2b5f7b0d33140af72c7f
Veylance <mail.veylance@gmail.com>
2025-06-02 23:57:04 +0800

    Feature: Custom Styling & Note Column

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const comp_inDmg        = document.getElementById("dps-inDmg");
 const comp_inAtkSpd     = document.getElementById("dps-inAtkSpd");
+const comp_isCustom     = document.getElementById("dps-isCustom");
 const compBtn_Calculate = document.getElementById("btn-Calculate");
 const comp_outTable     = document.getElementById("dps-outTable");
 
@@ -19,13 +20,17 @@ function appendDPS() {
         o5s:        document.createElement("td"),
         o10s:       document.createElement("td"),
         o30s:       document.createElement("td"),
-        o60s:       document.createElement("td")
+        o60s:       document.createElement("td"),
+        notes:      document.createElement("td")
     }
+    let inNotes     = document.createElement("textarea");
 
     // Append the <td> elements to the <tr>
     Object.keys(outTD).forEach((key) => {
         tr.appendChild(outTD[key]);
     });
+
+    if (comp_isCustom.checked) tr.classList.add("stats-custom");
 
     // Add content to the <td> elements
     outTD.stats.innerHTML = "D: " + comp_inDmg.value + " / AS: " + comp_inAtkSpd.value;
@@ -34,6 +39,9 @@ function appendDPS() {
     outTD.o10s.innerHTML  = this.calculateDPS(10).toFixed(2);
     outTD.o30s.innerHTML  = this.calculateDPS(30).toFixed(2);
     outTD.o60s.innerHTML  = this.calculateDPS(60).toFixed(2);
+    // Extra column for notes
+    outTD.notes.appendChild(inNotes);
+    inNotes.classList.add("form-control");
 
     // Append the <tr> to the output table
     comp_outTable.appendChild(tr);
